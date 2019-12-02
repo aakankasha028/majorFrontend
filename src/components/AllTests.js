ee185afdb2e330c97a2f716fb0d732c9cb333f91
krittikachhabra <krittikachhabra.becse16@pec.edu.in>
2019-12-03 00:13:33 +0530

    Test cards added

--- src/components/AllTests.js
+++ src/components/AllTests.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { Card, CardTitle, CardText, CardImg, CardImgOverlay } from 'reactstrap';
 import axios from 'axios';
 import Test from './Test.js';
+import { Link } from 'react-router-dom';
+import '../styles/cards.css';
 
 class AllTests extends Component {
     constructor(props) {
@@ -21,10 +24,36 @@ class AllTests extends Component {
         var tests = this.state.tests;
         return this.state.tests.length === 0 ?
             <div>Loading...</div> :
-            tests.map((test, key) => {
-                console.log(test);
-                return <Test testName={test["testName"]} completedDate={test["completedOn"]} />
-            });
+                tests.map((test, key) => {
+                        console.log(test);
+                        return (
+                            <div align="middle" className="col-lg-4 col-md-4">
+
+                                <div className="card">
+
+                                    <div className="view">
+                                        <img
+                                            src="https://www.news-medical.net/image.axd?picture=2019%2F3%2FGroup_of_friends_having_drinks_whiskey_at_the_night_club_after_work_._Young_men_drink_whiskey_and_enjoying_at_a_bar_toasting_cocktails-bluedog_studios.jpg"
+                                            className="card-img-top"
+                                            alt="photo"/>
+                                    </div>
+
+                                    <div className="card-body">
+                                        <h4 className="card-title"><Test testName={test["testName"]}
+                                                                         completedDate={test["completedOn"]}/></h4>
+
+                                        <p className="card-text"><i
+                                            title="Not completed yet means either you have not given the test or the last time you gave the test was long time ago, which means it has become stale">
+                                            What does "Not completed yet" mean?
+                                        </i></p>
+                                        <a href={"/tests?test=" + this.props.testName} className="btn btn-primary">Attempt
+                                            Test</a>
+                                    </div>
+                                </div>
+                            </div>
+                        );
+                    }
+                );
     }
 }
 
--- src/components/Test.js
+++ src/components/Test.js
@@ -6,9 +6,6 @@ class Test extends Component {
 			<div>
 				<h3>{this.props.testName}</h3>
 				<h4>Completed on: {this.props.completedDate === "00-00-0000" ? "Not completed yet" : this.props.completedDate}</h4>
-				<i title="Not completed yet means either you have not given the test or the last time you gave the test was long time ago, which means it has become stale">
-				What does "Not completed yet" mean?
-				</i>
 			{/*<a href="test/test={this.props.testName}">Attempt test</a>*/}
 			</div>
 			);
