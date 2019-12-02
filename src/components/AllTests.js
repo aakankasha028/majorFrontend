f97e8da70374235fe577d2ce96d9e6c02754612b
Aakankasha Sharma <akkutbgs3x@gmail.com>
2019-12-02 17:54:36 +0530

    all tests

--- src/components/AllTests.js
+++ src/components/AllTests.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import Test from './Test.js';
 
 class AllTests extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			tests: []
+		};
+	}
+
+	componentDidMount() {
+		axios.get("https://api.myjson.com/bins/1g475q").then(res => {
+			this.setState({tests: res.data.results});
+			console.log(this.state.tests);
+		})
+	}
+
 	render() {
-		return (
-			<h2>All tests page</h2>
-		);
+		var tests = this.state.tests;
+		return this.state.tests.length === 0 ? 
+		<div>Loading...</div> : 
+		tests.map((test, key) => {
+			console.log(test);
+			return <Test testName={test["testName"]} completedDate={test["completedOn"]} />
+		});
 	}
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
 import '../styles/Home.css';
+import { Link } from 'react-router-dom';
+
 class Home extends Component {
 	render() {
 		return(
-                    <div>
-                        <div className="background">
-                            <div className="transbox">
-                                <p>Just a test.
-                                    <br>
-                                    </br>
-                                    How you doin'?
-                                </p>
-                            </div>
-                        </div>
-                        {/*<h1>
-                    What is Lorem Ipsum?
+            <div>
+                <div className="background">
+                    <div className="transbox">
+                        <p>Just a test.
+                        <br>
+                        </br>
+                            How you doin'?
+                        </p>
+                    </div>
+                </div>
+                {/*<h1>
+                What is Lorem Ipsum?
                 </h1>
                 <p>
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
@@ -30,21 +32,8 @@ class Home extends Component {
                 </h1>
                 <p>
                     Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-                    The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-                </p>
-                <h1>
-                    Where can I get some?
-                </h1>
-                <p>
-                    There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
-                </p>
-                <h1>
-                    Where can I get some?
-                </h1>
-                <p>
-                    There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
                 </p>*/}
+			    <button><Link to={'login-register'}>Get Started</Link></button>
             </div>
 		);
 	}
@@ -52,9 +41,14 @@ class Home extends Component {
 
 export default Home;
 
+
+// if res fails to load
+// logout functionality
+// how to use conditionals and loops inside render - where to put {}
+// enclosing div tag ka error kyu nahi aaya in AllTests?
 // navbar - done
-// login / register
+// login / register - done
 // routes - done
 // all tests page
-// what to use for styling (CSS, Bootstrap, Materialize UI) - Read about Bootstrap, its features and Materialize
-// Sass
+// what to use for styling (CSS, Bootstrap, Materialize UI) - Read about Bootstrap, its features and Materialize - Bootstrap - done
+// Sass - done
