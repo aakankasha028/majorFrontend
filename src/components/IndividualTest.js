0a93cb510f4badfc3d0ebb176f95ac9d8cdb2658
aakankasha028 <akkutbgs3x@gmail.com>
2019-12-03 17:10:46 +0530

    added API call for get questionnaire in IndividualTest.js

--- src/components/IndividualTest.js
+++ src/components/IndividualTest.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import axios from 'axios';
 
 class IndividualTest extends Component {
@@ -8,22 +8,57 @@ class IndividualTest extends Component {
 		this.state = {
 			testName: "",
 			questionnaire: [],
-			responses: []
+			responses: {
+				testName:"",
+				response:[]
+			}
 		}
 	}
 
 	componentDidMount() {
 		var testName = this.props.location.search.split("=")[1];
-		{/*axios.get("").then(resp => {
-			console.log()
-		}*/}
+		axios.get("https://api.myjson.com/bins/qblgk").then(resp => {
+			// console.log(resp);
+			var responseArray = Array(resp.data.result.length);
+			this.setState({questionnaire: resp.data.result, response: responseArray});
+			// console.log(this.state.questionnaire);
+		});
+	}
+
+	handleResponses = (event, key) => {
+		var respArr = this.state.response;
+		respArr[key] = event.target.value;
+		this.setState({response: respArr});
+		// console.log(this.state.response);
 	}
 
 	render() {
 
 		return (
-			<h2>Individual Test Page</h2>
-			);
+			this.state.questionnaire.length === 0 ? 
+			<p>Loading...</p> :
+			this.state.questionnaire.map((obj, key) => {
+				return (
+					<Fragment>
+					<div id="question">{key+1}. {obj.question}</div>
+					{obj.responses.map((response, ikey) => {
+						return (
+							<Fragment>
+								<input 
+								type="radio"
+								name={"resp"+key}
+								value={response.name} 
+								onChange = {(e) => this.handleResponses(e, key)}
+							/> 
+								{response.value}
+								&emsp;
+							</Fragment>
+							);
+					})}
+					</Fragment>
+				)
+			})
+		);
 	}	
 }
 
